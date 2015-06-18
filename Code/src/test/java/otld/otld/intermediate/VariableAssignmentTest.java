c5989acd65b39efdde8761553b2c16ee9613ec13
Jan-Jelle Kester <janjelle@jjkester.nl>
2015-06-18 23:33:14 +0200

    Begin work on tests for intermediate representation

---
+++ Code/src/test/java/otld/otld/intermediate/VariableAssignmentTest.java
@@ -0,0 +1,32 @@
+package otld.otld.intermediate;
+
+import org.junit.Test;
+import otld.otld.intermediate.exceptions.TypeMismatch;
+
+import static org.junit.Assert.*;
+
+public class VariableAssignmentTest {
+
+    @Test
+    public void testGetDestination() throws Exception {
+        Variable v = Variable.create(Type.INT, "v", null);
+        Variable w = Variable.create(Type.INT, "w", "42");
+        VariableAssignment vw = new VariableAssignment(v, w);
+        assertEquals(v, vw.getDestination());
+    }
+
+    @Test
+    public void testGetSource() throws Exception {
+        Variable v = Variable.create(Type.INT, "v", null);
+        Variable w = Variable.create(Type.INT, "w", "42");
+        VariableAssignment vw = new VariableAssignment(v, w);
+        assertEquals(w, vw.getSource());
+    }
+
+    @Test(expected = TypeMismatch.class)
+    public void testGetSourceError() throws Exception {
+        Variable v = Variable.create(Type.INT, "v", null);
+        Variable w = Variable.create(Type.BOOL, "w", "false");
+        VariableAssignment vw = new VariableAssignment(v, w);
+    }
+}
