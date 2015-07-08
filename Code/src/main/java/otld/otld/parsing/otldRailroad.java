835fe9d973de2ca2c7330912c78c149a13cdbe03
TheMSB <Martijn.bruning@gmail.com>
2015-07-08 14:30:53 +0200

    Visitor updates

--- Code/src/main/java/otld/otld/parsing/otldRailroad.java
+++ Code/src/main/java/otld/otld/parsing/otldRailroad.java
@@ -1,17 +1,17 @@
 package otld.otld.parsing;
 
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
 import org.antlr.v4.runtime.tree.TerminalNode;
 import otld.otld.grammar.otldBaseListener;
 import otld.otld.grammar.otldParser;
-import otld.otld.intermediate.Function;
-import otld.otld.intermediate.Program;
-import otld.otld.intermediate.Type;
-import otld.otld.intermediate.Variable;
+import otld.otld.intermediate.*;
 import otld.otld.intermediate.exceptions.FunctionAlreadyDeclared;
+import otld.otld.intermediate.exceptions.TypeMismatch;
 import otld.otld.intermediate.exceptions.VariableAlreadyDeclared;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Base visitor for the OTLD intermediate representation.
@@ -19,6 +19,8 @@ import java.util.List;
 public class otldRailroad extends otldBaseListener {
 
     private Program city;
+    ParseTreeProperty<Function> functions;
+    Stack<OperationSequence> stack;
 
     public Type getType(String ctxType) {
 
@@ -59,21 +61,20 @@ public class otldRailroad extends otldBaseListener {
     }
 
     @Override
-    public void exitDeffactory(otldParser.DeffactoryContext ctx) {
-        super.exitDeffactory(ctx);
-    }
-
-    @Override
     public void enterProgram(otldParser.ProgramContext ctx) { super.enterProgram(ctx); }
 
     @Override
-    public void exitProgram(otldParser.ProgramContext ctx) {
-        super.exitProgram(ctx);
-    }
+    public void exitProgram(otldParser.ProgramContext ctx) { super.exitProgram(ctx); }
 
     @Override
     public void enterCity(otldParser.CityContext ctx) {
         city = new Program(ctx.ID().getText());
+        stack.push(city.getBody());
+    }
+
+    @Override
+    public void exitCity(otldParser.CityContext ctx) {
+        stack.pop();
     }
 
     @Override
@@ -104,19 +105,37 @@ public class otldRailroad extends otldBaseListener {
             types.add(getType(n.getText()));
         }
 
-        Function func = new Function(ctx.ID().getText(), (Type[]) types.toArray());
-    }
-
-    @Override
-    public void exitFactory(otldParser.FactoryContext ctx) {
-        super.exitFactory(ctx);
-        //TODO
+        try {
+            Function function = new Function(ctx.ID().getText(), (Type[]) types.toArray());
+            city.addFunction(function);
+            functions.put(ctx.deffactory(), function);
+        } catch (FunctionAlreadyDeclared functionAlreadyDeclared) {
+            System.out.println("Function has already been declared");
+            //TODO error handling
+        }
     }
 
     @Override
     public void enterDeffactory(otldParser.DeffactoryContext ctx) {
-        super.enterDeffactory(ctx);
-        //TODO
+        Function function = functions.get(ctx);
+        stack.push(function.getBody());
+
+    }
+
+    @Override
+    public void exitDeffactory(otldParser.DeffactoryContext ctx) {
+        Return ret;
+        if (city.getVariable(ctx.ID().getText())!= null) {
+            ret = new Return(city.getVariable(ctx.ID().getText()));
+            if (!functions.get(ctx).getType().equals(ret.getSource().getType())) {
+                // TODO handle type error
+            }
+            stack.peek().add(ret);
+        } else {
+            // TODO Handle error variable undeclared.
+        }
+
+        stack.pop();
     }
 
     @Override
@@ -134,9 +153,52 @@ public class otldRailroad extends otldBaseListener {
 
         try {
             city.addFunction(new Function(ctx.ID().getText(), Type.BOOL));
+            city.addVariable(Variable.create(Type.BOOL, ctx.ID().getText(), "false"));
+            functions.put(ctx.ID(), null);
         } catch (FunctionAlreadyDeclared functionAlreadyDeclared) {
             System.out.println("Function has already been declared");
             //TODO error handling
+        } catch (VariableAlreadyDeclared variableAlreadyDeclared) {
+            System.out.println("Variable has already been declared");
+            //TODO error handling
         }
     }
+
+    @Override
+    public void exitDefwaypoint(otldParser.DefwaypointContext ctx) {
+        Return ret;
+        if (city.getVariable(ctx.ID().getText())!= null) {
+            ret = new Return(city.getVariable(ctx.ID().getText()));
+            if (!functions.get(ctx).getType().equals(ret.getSource().getType())) {
+                // TODO handle type error
+            }
+            stack.peek().add(ret);
+        } else {
+            // TODO Handle error variable undeclared.
+        }
+        stack.pop();
+    }
+
+    @Override
+    public void enterDefcircle(otldParser.DefcircleContext ctx) {
+        Variable variable = city.getVariable(ctx.ID().getText());
+        if (variable != null) {
+            try {
+                stack.peek().add(new Call(city.getFunction(ctx.ID().getText())));
+                Loop loop = new Loop(variable);
+                stack.peek().add(loop);
+                stack.push(loop.getBody());
+
+            } catch (TypeMismatch typeMismatch) {
+                System.out.println("Type mismatch!");
+                //TODO error handling
+            }
+        }
+    }
+
+    @Override
+    public void exitDefcircle(otldParser.DefcircleContext ctx) {
+        stack.pop();
+    }
+
 }
